c712d9945f153be85e7de6e0e05a63c4e557b7cb
davinahouquet <davina.houquet@hotmail.fr>
2023-10-10 15:50:25 +0200

    Affichage des espaces + espace grâce à l'id

--- src/Controller/EspaceController.php
+++ src/Controller/EspaceController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Espace;
+use App\Repository\EspaceRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EspaceController extends AbstractController
 {
     #[Route('/espace', name: 'app_espace')]
-    public function index(): Response
+    public function index(EspaceRepository $espaceRepository): Response
     {
+        $espaces = $espaceRepository->findAll();
+
         return $this->render('espace/index.html.twig', [
-            'controller_name' => 'EspaceController',
+            'espaces' => $espaces
+        ]);
+    }
+
+    #[Route('/espace/{id}', name: 'show_espace')]
+    public function show_espace(Espace $espace): Response
+    {
+        return $this->render('espace/show.html.twig', [
+            'espace' => $espace
         ]);
     }
 }
