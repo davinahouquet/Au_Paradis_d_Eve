b81b6df43794d0d8e6dac07126e268feb434c2c8
davinahouquet <davina.houquet@hotmail.fr>
2023-10-11 16:58:54 +0200

    Formulaire de réservation chambre

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -2,17 +2,46 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ReservationType;
+use App\Repository\EspaceRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ReservationController extends AbstractController
 {
     #[Route('/reservation', name: 'app_reservation')]
-    public function index(): Response
+    public function index(EspaceRepository $espaceRepository): Response
     {
+
+        $chambres = $espaceRepository->findByCategorie(1);
+
         return $this->render('reservation/index.html.twig', [
-            'controller_name' => 'ReservationController',
+            'chambres' => $chambres,
         ]);
     }
+    // rajouter un {id} dans cette route
+    #[Route('/newReservation/', name: 'new_reservation')]
+    public function newReservation(EntityManagerInterface $entityManager, Request $request)
+    {
+
+        $form = $this->createForm(ReservationType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+    
+            $reservation = $form->getData();
+                
+            $entityManager->persist($reservation);
+            $entityManager->flush();
+    
+            $this->addFlash('message', 'La réservation a bien été prise en compte');
+            return $this->redirectToRoute('app__reservation');
+            }
+    
+            return $this->render('reservation/new.html.twig', [
+                'form' => $form
+            ]);
+        }
 }
---
+++ src/Form/ReservationType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Entity\Espace;
+use App\Entity\Reservation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+
+class ReservationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('prenom', TextType::class, [
+            'label' => 'Prénom',
+        ])
+        ->add('nom', TextType::class, [
+            'label' => 'Nom',
+        ])
+        ->add('telephone', TextType::class, [
+            'label' => 'Téléphone',
+        ])
+        ->add('nb_personnes', NumberType::class, [
+            'label' => 'Nombre de personnes',
+        ])
+        ->add('date_debut', DateType::class, [
+            'label' => 'Date de début',
+        ])
+        ->add('date_fin', DateType::class, [
+            'label' => 'Date de fin',
+        ])
+        // ->add('prixTotal', NumberType::class, [
+        //     'label' => 'Prix total',
+        // ])
+        ->add('options', TextType::class, [
+            'label' => 'Options',
+        ])
+        // ->add('note', TextType::class, [
+        //     'label' => 'Note',
+        // ])
+        // ->add('avis', TextType::class, [
+        //     'label' => 'Avis',
+        // ])
+        ->add('espace', EntityType::class, [
+            'class' => Espace::class,
+            'choice_label' => 'nomEspace', 
+            'label' => 'Chambre',
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Reservation::class,
+        ]);
+    }
+}
