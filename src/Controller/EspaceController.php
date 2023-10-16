db2c4c321fe96ca1d2047d1e2a614e89e5d4bf09
davinahouquet <davina.houquet@hotmail.com>
2023-10-16 16:21:00 +0200

    Ajout lien résa pour chambres uniquement

--- src/Controller/EspaceController.php
+++ src/Controller/EspaceController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Espace;
 use App\Repository\EspaceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,10 +22,14 @@ class EspaceController extends AbstractController
     }
 
     #[Route('/espace/{id}', name: 'show_espace')]
-    public function show_espace(Espace $espace): Response
+    public function show_espace(Espace $espace, EspaceRepository $espaceRepository, EntityManagerInterface $entityManager): Response
     {
+        $repository = $entityManager->getRepository(Espace::class);
+        $chambre = $repository->findByCategorie(1);
+
         return $this->render('espace/show.html.twig', [
-            'espace' => $espace
+            'espace' => $espace,
+            'chambre' => $chambre
         ]);
     }
 }
