0a677201f148e94f900208837522279eb47198cf
davinahouquet <davina.houquet@hotmail.com>
2023-10-16 08:44:03 +0200

    Lien de redirection vérification email

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -88,8 +88,8 @@ class RegistrationController extends AbstractController
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->addFlash('success', 'Votre email a été vérifié.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('app_login');
     }
 }
