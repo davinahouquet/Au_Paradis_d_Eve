5728c9a4731f53ba90e2add6b081dcd3a920118f
davinahouquet <davina.houquet@hotmail.com>
2023-10-16 11:54:10 +0200

    ToString user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -188,4 +188,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString(){
+        return $this->adresse." ".$this->cp;
+    }
 }
