7558d647d6bdaf7bc2309e6a16253d59fc0bc35f
davinahouquet <davina.houquet@hotmail.com>
2023-10-16 11:53:39 +0200

    Traduction fr

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -26,7 +26,7 @@ class RegistrationFormType extends AbstractType
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Vous devriez accepter ces conditions.',
                     ]),
                 ],
             ])
@@ -37,11 +37,11 @@ class RegistrationFormType extends AbstractType
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez entrer un mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit contenir {{ limit }} caractères minimum',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
