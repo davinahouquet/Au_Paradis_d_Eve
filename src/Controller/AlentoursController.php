3808562efdd06f5435d3f0dd6621ec119b4c5e4e
davinahouquet <davina.houquet@hotmail.com>
2023-11-01 23:02:33 +0100

    Affichage des alentours

--- src/Controller/AlentoursController.php
+++ src/Controller/AlentoursController.php
@@ -5,14 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AlentoursController extends AbstractController
 {
     #[Route('/alentours', name: 'app_alentours')]
     public function index(): Response
     {
+        $jsonData = file_get_contents('../public/json/alentours.json');
+        $data = json_decode($jsonData, true);
+
         return $this->render('alentours/index.html.twig', [
-            'controller_name' => 'AlentoursController',
+            'data' => $data
         ]);
     }
 }
