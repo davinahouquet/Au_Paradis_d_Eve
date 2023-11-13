60251070d1b0468a379ad2af6b990a71c1734512
davinahouquet <davina.houquet@hotmail.com>
2023-11-13 07:44:49 +0100

    Espacement

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -83,7 +83,6 @@ class RegistrationController extends AbstractController
             $this->emailVerifier->handleEmailConfirmation($request, $user);
         } catch (VerifyEmailExceptionInterface $exception) {
             $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
-
             return $this->redirectToRoute('app_register');
         }
 
