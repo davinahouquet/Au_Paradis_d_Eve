a7ca014eaac3a7731075e8cb7659c553378c9bb6
davinahouquet <davina.houquet@hotmail.com>
2023-12-24 14:08:55 +0100

    Backup du formulaire de réservation

---
+++ src/Form/ReservationType.bak.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Option;
+use App\Entity\Reservation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+
+class ReservationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('prenom', TextType::class, [
+            'label' => 'Prénom*',
+            'required' => true,
+            'attr' => [
+                'value' => 'Stewie'
+            ]
+        ])
+        ->add('nom', TextType::class, [
+            'label' => 'Nom*',
+            'required' => true,
+            'attr' => [
+                'value' => 'Griffin'
+            ]
+        ])
+        ->add('telephone', TextType::class, [
+            'label' => 'Téléphone*',
+            'required' => true,
+            'attr' => [
+                'value' => '444719'
+            ]
+        ])
+        ->add('nb_personnes', NumberType::class, [
+            'label' => 'Nombre de personnes*',
+            'required' => true,
+            'attr' => [
+                'value' => 1
+            ]
+        ])
+        ->add('date_debut', DateType::class, [
+            'widget' => 'single_text',
+            'required' => true,
+            'attr' => [
+                'class' => 'form-control',
+                // 'min' => ( new \DateTime() )->format('Y-m-d H:i:s')
+            ],
+            'label' => 'Date de début*',
+            'data' => new \DateTime()
+        ])
+        ->add('date_fin', DateType::class, [
+            'widget' => 'single_text',
+            'required' => true,
+            'attr' => [
+                'class' => 'form-control'
+            ],
+            'label' => 'Date de fin*'
+        ])
+        ->add('options', CollectionType::class, [
+            // each entry in the array will be an "email" field
+            'entry_type' => Option::class,
+            // these options are passed to each "email" type
+        ])
+        ->add('valider', SubmitType::class, [
+            'attr' => [
+            'class' => 'btn btn-success'
+            ]
+        ]); 
+        ;
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Reservation::class,
+            'options' => []
+        ]);
+    }
+
+
+    private function getChoices($options)
+    {
+        $choices = [];
+        foreach ($options as $key => $data) {
+            $choices[$data['nom'] . ' '. $data['tarif'] . '€'] = $key;
+        }
+
+        return $choices;
+    }
+}
