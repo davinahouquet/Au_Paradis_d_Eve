b5cb695b4a50688e17a571c43da49236f7e6d496
davinahouquet <davina.houquet@hotmail.com>
2024-01-03 08:50:16 +0100

    Suppression du calcul prix options

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -60,24 +60,24 @@ class ReservationController extends AbstractController
         }
     }
 
-    private function calculerPrixOptions($selectedOptions)
-    {
-        $optionsFile = file_get_contents('../public/json/options.json');
-        $optionsData = json_decode($optionsFile, true);
+    // private function calculerPrixOptions($selectedOptions)
+    // {
+    //     $optionsFile = file_get_contents('../public/json/options.json');
+    //     $optionsData = json_decode($optionsFile, true);
     
-        $prixTotalOptions = 0;
+    //     $prixTotalOptions = 0;
     
-        foreach ($selectedOptions as $selectedOption) {
-            foreach ($optionsData['options_chambres_hotes'] as $optionData) {
-                if ($optionData['id_option'] == $selectedOption) {
-                    $prixTotalOptions += $optionData['tarif_option'];
-                    break;
-                }
-            }
-        }
+    //     foreach ($selectedOptions as $selectedOption) {
+    //         foreach ($optionsData['options_chambres_hotes'] as $optionData) {
+    //             if ($optionData['id_option'] == $selectedOption) {
+    //                 $prixTotalOptions += $optionData['tarif_option'];
+    //                 break;
+    //             }
+    //         }
+    //     }
     
-        return $prixTotalOptions;
-    }
+    //     return $prixTotalOptions;
+    // }
     
     // Ajouter une réservation OU modifier
     #[Route('/reservation/new/{id}', name: 'new_reservation')]
@@ -93,57 +93,25 @@ class ReservationController extends AbstractController
 
         $chambre = $espace->getNomEspace();
         
-        $options = $this->reservationService->getAllOptionsFromJson();
+        // $options = $this->reservationService->getAllOptionsFromJson();
     
-        $form = $this->createForm(ReservationType::class, $reservation, ['options' => $options]);
+        $form = $this->createForm(ReservationType::class, $reservation);
         // $form = $this->createForm(ReservationType::class, $reservation);
             
         $form->handleRequest($request);
 
-            if ($form->isSubmitted() && $form->isValid()) {
-            
-            $reservation = $form->getData();
-            $reservation->setEspace($espace);
+        if ($form->isSubmitted() && $form->isValid()) {
+        
+        $reservation = $form->getData();
+        $reservation->setEspace($espace);
 
-            //Trouver les dates de début et de fin de la réservation en cours
-            $dateDebutNlleReservation = $reservation->getDateDebut();
-            $dateFinNlleReservation = $reservation->getDateFin();
-
-            //Trouver les espaces réservées aux dates sélectionnées grâce à la requête DQL créée dans ReservationRepository
-            $indisponible = $reservationRepository->findEspacesReserves($espace, $dateDebutNlleReservation, $dateFinNlleReservation);
-
-            //Toutes les conditions nécessaires pour poursuivre la réservation      
-                    // //la date du jour
-            date_default_timezone_set('Europe/Paris');                       
-            $currentDate = new \Datetime();
-            if($dateDebutNlleReservation > $dateFinNlleReservation){ //La date de début du séjour doit être supérieure à sa date de fin
-                $this->addFlash('message', 'La date de fin de votre séjour doit être supérieure à sa date de début.');
-                return $this->redirectToRoute('show_espace', ['id' => $espace->getId()]);
-            } elseif($indisponible){ //Pas de chevauchement de réservation
-                $this->addFlash('message', 'La réservation se chevauche avec une réservation existante. Veuillez choisir d\'autres dates.');
-                return $this->redirectToRoute('show_espace', ['id' => $espace->getId()]);
-            } elseif($reservation->getDateDebut() <= $currentDate){ //Pas de réservation dans le passé, ni au jour même
-                $this->addFlash('message', 'Merci de réserver au moins un jour avant le début du séjour');
-                return $this->redirectToRoute('show_espace', ['id' => $espace->getId()]);
-            } elseif($reservation->getDuree() < 2 ){  //Minimum 2 jours pour une réservation
-                $this->addFlash('message', 'La réservation doit être de deux nuits minimum');
-                return $this->redirectToRoute('show_espace', ['id' => $espace->getId()]);
-            } elseif($reservation->getDuree() > 28 ) { //Maximum 28 jours pour une réservation
-                $this->addFlash('message', 'La réservation ne doit pas excéder 28 jours');
-                return $this->redirectToRoute('show_espace', ['id' => $espace->getId()]);
-            } elseif($reservation->getNbPersonnes() > $espace->getNbPlaces()) { //Le nombre de personnes dans la réservation ne doit pas excéder le nombre de places dans la chambre (hors enfants)
-                $this->addFlash('message', 'Nombre de personnes trop élevé. Merci de réserver une autre chambre pour les personnes supplémentaires. (Hors enfants)');
-                return $this->redirectToRoute('show_espace', ['id' => $espace->getId()]);
-            } else { //Si toutes les conditions sont réunies, alors on peut poursuivre la réservation et insérer les champs en base de données
-            // Set options dans reservation
-            $selectedOptions = $form->get('options')->getData();
-            if (is_array($selectedOptions)) {
-                $reservation->setOptions($selectedOptions);
-
-                // Calculer le prix total des options et l'assigner à l'entité Reservation
-                // $prixTotalOptions = $this->calculerPrixOptions($selectedOptions);
-                // $reservation->setPrixOptions($prixTotalOptions);
-            }
+        // verifie si la chambre est disponible
+        $message = $this->reservationService->verifierDisponibiliteChambre($espace, $reservation);
+         // si le message est null, alors erreur de disponiblité
+            if ($message !== null) {
+                $this->addFlash('danger', $message);
+                return $this->redirectToRoute('new_reservation', ['id' => $espace->getId()]);
+            } else {
                 $entityManager->persist($reservation);
                 $entityManager->flush();
                 
@@ -151,13 +119,15 @@ class ReservationController extends AbstractController
                 $this->addFlash('message', 'Les informations ont bien été prises en compte, vous allez passer à l\'étape suivante...');
                 return $this->redirectToRoute('new_coordonnees', ['reservation' => $reservation->getId()]);
             }
-        }  
-            return $this->render('reservation/new.html.twig', [
-                'form' => $form,
-                'chambre' => $chambre,
-                'options' => $options
-            ]);
         }
+        
+        return $this->render('reservation/new.html.twig', [
+            'form' => $form,
+            'chambre' => $chambre,
+            // 'options' => $options
+        ]);
+    }
+
 
         #[Route('/reservation/choix/{id}', name:'app_choix') ]
         public function choix(Espace $espace, EspaceRepository $espaceRepository, EntityManagerInterface $entityManager, Request $request)
