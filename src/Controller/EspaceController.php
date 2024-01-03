06925d36387ed40502236220759c77ed4bc07b5f
davinahouquet <davina.houquet@hotmail.com>
2024-01-03 08:45:03 +0100

    Formulaire d'ajout d'images dans le contrôleur de l'espace

--- src/Controller/EspaceController.php
+++ src/Controller/EspaceController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Image;
 use App\Entity\Espace;
+use App\Form\ImageType;
 use App\Form\EspaceType;
 use App\Repository\EspaceRepository;
 use App\Repository\CategorieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+// use Symfony\Component\Validator\Constraints\Image;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -35,31 +38,66 @@ class EspaceController extends AbstractController
 
     #[Route('/espace/new', name: 'new_espace')]
     #[Route('/espace/edit/{id}', name: 'edit_espace')] 
-    public function new_edit_espace(Espace $espace = null, CategorieRepository $categorieRepository, Request $request, EntityManagerInterface $entityManager): Response
+    public function new_edit_espace(Espace $espace = null, Image $image = null, CategorieRepository $categorieRepository, Request $request, EntityManagerInterface $entityManager): Response
     {
+        // dd($espace->getImages());
         if(!$espace){
             $espace = new Espace();
         }
         $categories = $categorieRepository->findAll();
 
         $form = $this->createForm(EspaceType::class, $espace);
+        // $formImage = $this->createForm(ImageType::class, $image);
 
         $form->handleRequest($request);
+        // $formImage->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            
-            $espace = $form->getData();
-            // dd($espace);
-            $entityManager->persist($espace);
-            $entityManager->flush();
 
-            $this->addFlash('success', 'Espace ajouté');
+            // dump();
+            // if($formImage->isSubmitted() && $formImage->isValid()) {
+                // $images = $form['images']->getData(); 
+                // dd($images);
+                $espace = $form->getData();
 
-            return $this->redirectToRoute('app_espace');
+                $images = $form->get('images')->getData();
+
+                foreach ($images as $imageFile) {
+                    // Create a new Image entity
+                    $image = new Image();
+
+                    // Generate a unique filename and set it as the link
+                    $fileName = md5(uniqid()) . '.' . $imageFile->guessExtension();
+                    $imageFile->move(
+                        $this->getParameter('../public/img/espaces/'), // Set your images directory parameter
+                        $fileName
+                    );
+
+                    // Set the link and alt attributes
+                    $image->setLienImage($fileName);
+                    $image->setAltImage('Alt description'); // You can customize this as needed
+
+                    // Associate the image with the Espace entity
+                    $image->setEspace($espace);
+                
+                // dd($form->getData());
+                // dd($espace);
+                // $espace->addImage();
+
+                $entityManager->persist($espace);
+                $entityManager->flush();
+    
+                $this->addFlash('success', 'Espace ajouté');
+    
+                return $this->redirectToRoute('app_espace');
+                }
+            // }
+            // dump();
         }
 
         return $this->render('espace/new.html.twig', [
             'form' => $form,
+            // 'formImage' => $formImage,
             'categories' => $categories,
         ]);
     }
