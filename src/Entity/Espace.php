176ad71746c29c9e14c7b82265a2549b55880ce6
davinahouquet <davina.houquet@hotmail.com>
2024-01-05 11:22:58 +0100

    Ajout propriété description

--- src/Entity/Espace.php
+++ src/Entity/Espace.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\EspaceRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: EspaceRepository::class)]
@@ -39,6 +40,9 @@ class Espace
 
     #[ORM\OneToMany(mappedBy: 'espace', targetEntity: Image::class)]
     private Collection $images;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
 
     public function __construct()
     {
@@ -185,5 +189,17 @@ class Espace
 
     public function __toString(){
         return $this->nom_espace." ".$this->taille." ".$this->wifi." ".$this->nb_places." ".$this->prix;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
     }
 }
