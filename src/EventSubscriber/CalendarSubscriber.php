a6230bed982c2b9446c79704a8f692c7db677d13
davinahouquet <davina.houquet@hotmail.com>
2024-01-19 12:54:44 +0100

    Ajout FullCalendar

---
+++ src/EventSubscriber/CalendarSubscriber.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\Espace;
+use App\Entity\Reservation;
+use CalendarBundle\Entity\Event;
+use CalendarBundle\CalendarEvents;
+use App\Repository\BookingRepository;
+use CalendarBundle\Event\CalendarEvent;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class CalendarSubscriber implements EventSubscriberInterface
+{
+    public function __construct(
+        private BookingRepository $bookingRepository,
+        private UrlGeneratorInterface $router
+    )
+    {}
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            CalendarEvents::SET_DATA => 'onCalendarSetData',
+        ];
+    }
+
+    public function onCalendarSetData(CalendarEvent $calendar, Reservation $reservation, Espace $espace)
+    {
+        $start = $calendar->getStart();
+        $end = $calendar->getEnd();
+        // $start = $reservation->getDateDebut();
+        // $end = $reservation->getDateFin();
+
+        // $start = $reservation->getStart();
+        // $end = $calendar->getEnd();
+        
+        $filters = $calendar->getFilters();
+
+        // Modify the query to fit to your entity and needs
+        // Change booking.beginAt by your start date property
+        $bookings = $this->bookingRepository
+            ->createQueryBuilder('booking')
+            ->where('booking.beginAt BETWEEN :start and :end OR booking.endAt BETWEEN :start and :end')
+            ->setParameter('start', $start->format('Y-m-d H:i:s'))
+            ->setParameter('end', $end->format('Y-m-d H:i:s'))
+            ->getQuery()
+            ->getResult()
+        ;
+
+        foreach ($bookings as $booking) {
+            // this create the events with your data (here booking data) to fill calendar
+            $bookingEvent = new Event(
+                $booking->getTitle(),
+                $booking->getBeginAt(),
+                $booking->getEndAt() // If the end date is null or not defined, a all day event is created.
+            );
+
+            /*
+             * Add custom options to events
+             *
+             * For more information see: https://fullcalendar.io/docs/event-object
+             * and: https://github.com/fullcalendar/fullcalendar/blob/master/src/core/options.ts
+             */
+
+            $bookingEvent->setOptions([
+                'backgroundColor' => 'red',
+                'borderColor' => 'red',
+            ]);
+            $bookingEvent->addOption(
+                'url',
+                $this->router->generate('app_booking_show', [
+                    'id' => $booking->getId(),
+                ])
+            );
+
+            // finally, add the event to the CalendarEvent to fill the calendar
+            $calendar->addEvent($bookingEvent);
+        }
+    }
+}
