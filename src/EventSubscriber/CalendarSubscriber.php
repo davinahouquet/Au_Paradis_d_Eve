9d062a22fd4485200c3299a12b1cc2acf1f2a58f
davinahouquet <davina.houquet@hotmail.com>
2024-01-20 00:14:26 +0100

    WIP calendarBundle Subscriber

--- src/EventSubscriber/CalendarSubscriber.php
+++ src/EventSubscriber/CalendarSubscriber.php
@@ -1,23 +1,26 @@
 <?php
 
 namespace App\EventSubscriber;
-
-use App\Entity\Espace;
-use App\Entity\Reservation;
 use CalendarBundle\Entity\Event;
 use CalendarBundle\CalendarEvents;
-use App\Repository\BookingRepository;
+use App\Repository\EspaceRepository;
 use CalendarBundle\Event\CalendarEvent;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class CalendarSubscriber implements EventSubscriberInterface
 {
+    private RequestStack $requestStack;
+
     public function __construct(
-        private BookingRepository $bookingRepository,
-        private UrlGeneratorInterface $router
+        private EspaceRepository $espaceRepository,
+        private UrlGeneratorInterface $router,
+        RequestStack $requestStack
     )
-    {}
+    {
+        $this->requestStack = $requestStack;
+    }
 
     public static function getSubscribedEvents()
     {
@@ -28,42 +31,34 @@ class CalendarSubscriber implements EventSubscriberInterface
 
     public function onCalendarSetData(CalendarEvent $calendar)
     {
-        dd('ok');
         $start = $calendar->getStart();
         $end = $calendar->getEnd();
-        // $start = $reservation->getDateDebut();
-        // $end = $reservation->getDateFin();
-
-        // $start = $reservation->getStart();
-        // $end = $calendar->getEnd();
         
         $filters = $calendar->getFilters();
+        // Récupérer la requête actuelle
+        $request = $this->requestStack->getCurrentRequest();
 
-        // Modify the query to fit to your entity and needs
-        // Change booking.beginAt by your start date property
-        $bookings = $this->bookingRepository
-            ->createQueryBuilder('booking')
-            ->where('booking.beginAt BETWEEN :start and :end OR booking.endAt BETWEEN :start and :end')
-            ->setParameter('start', $start->format('Y-m-d H:i:s'))
-            ->setParameter('end', $end->format('Y-m-d H:i:s'))
+        // Récupérer le chemin de l'URL référente
+        $refererPathInfo = $request->headers->get('referer');
+
+        // Utiliser Symfony pour extraire le paramètre 'id' du chemin de l'URL
+        $segments = explode('/', trim($refererPathInfo, '/'));
+        $espaceId = end($segments);
+
+        $espace = $this->espaceRepository
+            ->createQueryBuilder('e')
+            ->where('e.id = :espaceId')
+            ->setParameter('espaceId', $espaceId)
             ->getQuery()
-            ->getResult()
+            ->getSingleResult();
         ;
-
-        foreach ($bookings as $booking) {
-            // this create the events with your data (here booking data) to fill calendar
+dd($_SESSION);
+        foreach ($espace->getReservations() as $reservation) {
             $bookingEvent = new Event(
-                $booking->getTitle(),
-                $booking->getBeginAt(),
-                $booking->getEndAt() // If the end date is null or not defined, a all day event is created.
+                $_SESSION['user']['role'] = 'ROLE_ADMIN' ? $reservation->getNom() . ' ' . $reservation->getPrenom()  : "Reservé",
+                $reservation->getDateDebut(),
+                $reservation->getDateFin()
             );
-
-            /*
-             * Add custom options to events
-             *
-             * For more information see: https://fullcalendar.io/docs/event-object
-             * and: https://github.com/fullcalendar/fullcalendar/blob/master/src/core/options.ts
-             */
 
             $bookingEvent->setOptions([
                 'backgroundColor' => 'red',
@@ -72,11 +67,10 @@ class CalendarSubscriber implements EventSubscriberInterface
             $bookingEvent->addOption(
                 'url',
                 $this->router->generate('app_booking_show', [
-                    'id' => $booking->getId(),
+                    'id' => $reservation->getId(),
                 ])
             );
 
-            // finally, add the event to the CalendarEvent to fill the calendar
             $calendar->addEvent($bookingEvent);
         }
     }
