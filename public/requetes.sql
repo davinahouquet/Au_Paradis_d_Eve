7de00ed0ed10210559ec5dfdc0df011dd93f7127
davinahouquet <davina.houquet@hotmail.com>
2024-01-23 08:43:26 +0100

    Reqiêtes SQL ajout réservation

---
+++ public/requetes.sql
@@ -0,0 +1,8 @@
+INSERT INTO reservation (prenom, nom, telephone, nb_personnes, date_debut, date_fin, prixTotal, note, avis, espace_id, email, adresseFacturation, facture, dateReservation, user_id, statut)
+VALUES ('John', 'Doe', '123456789', 2, '2023-01-01', '2023-01-10', 500.00, 4.5, 'Très satisfait', 1, 'john.doe@example.com', '123 Main St', 'INV123', '2022-12-15', 1, 'CONFIRMEE');
+
+INSERT INTO reservation (prenom, nom, telephone, nb_personnes, date_debut, date_fin, prixTotal, note, avis, espace_id, email, adresseFacturation, facture, dateReservation, user_id, statut)
+VALUES ('Jane', 'Doe', '5551234567', 1, '2022-05-15', '2022-05-20', 300.00, 3.5, 'Satisfaisant', 3, 'jane.doe@example.com', '789 Pine St', 'INV789', '2022-04-25', 3, 'Terminée');
+
+INSERT INTO reservation (prenom, nom, telephone, nb_personnes, date_debut, date_fin, prixTotal, note, avis, espace_id, email, adresseFacturation, facture, dateReservation, user_id, statut)
+VALUES ('Alice', 'Smith', '987654321', 3, '2030-12-01', '2030-12-10', 800.00, 4.0, 'Bon séjour', 2, 'alice.smith@example.com', '456 Oak St', 'INV456', '2022-12-15', 2, 'CONFIRMEE');
