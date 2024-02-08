095b4eff20dafc6fe13c6e6ff506a7aa723188d3
davina.houquet <davina.houquet@hotmail.com>
2024-02-08 21:42:56 +0100

    Regex chgt mdp

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,31 +20,30 @@ class ChangePasswordFormType extends AbstractType
         $builder
         ->add('plainPassword', RepeatedType::class, [
             'type' => PasswordType::class,
-            'options' => [
-                'attr' => [
-                    'autocomplete' => 'new-password',
-                    'class' => 'form-control'
-                ],
-            ],
-            'first_options' => [
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Votre mot de passe doit avoir 6 caractères au minimum',
-                        //Longueur maximum autorisée par Symfony pour des raisons de sécurité
-                        'max' => 4096,
-                    ]),
-                ],
-                'label' => 'Nouveau mot de passe',
-            ],
-            'second_options' => [
-                'label' => 'Confirmer le nouveau mot de passe',
-            ],
-            'invalid_message' => 'Les mots de passes doivent être similaires',
             'mapped' => false,
+            'invalid_message' => 'Les mots de passe doivent correspondre.',
+            'options' => ['attr' => ['class' => 'password-field']],
+            'required' => true,
+            'first_options'  => ['label' => 'Nouveau mot de passe',
+                'attr' => ['class' => 'form-control']
+            ],
+            'second_options' => ['label' => 'Confirmez le nouveau mot de passe',
+            'attr' => ['class' => 'form-control']        
+            ],
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'Veuillez entrer un mot de passe',
+                ]),
+                new Length([
+                    'min' => 12,
+                    'minMessage' => 'Votre mot de passe doit contenir 12 caractères minimum',
+                    'max' => 4096,
+                ]),
+                new Regex([
+                    'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/',
+                    'message' => 'Le mot de passe doit contenir au moins une minuscule, une majuscule, un chiffre et un caractère spécial.'
+                ]),
+            ],
         ])
         ->add('valider', SubmitType::class, [
             'attr' => [
